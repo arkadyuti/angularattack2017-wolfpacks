127086e1b2ce2304076bf4fd3f6ef3f166bb6402
Veeresh Hiremath <vhiremath4@sapient.com>
2017-04-24 04:36:16 +0530

    User Profile

--- src/app/user/signUp/sign-up.component.ts
+++ src/app/user/signUp/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+
 
 
 @Component({
@@ -8,15 +10,35 @@ import { Router } from '@angular/router';
 })
 
 export class SignUpComponent {
+
   email: string;
   password1: string;
   password2: string;
   passwordFail: boolean = false;
 
-  constructor( private router: Router){}
+  constructor( private router: Router, private af: AngularFire ){}
 
+  signUp(){
+
+    if (this.password1 !== this.password2) {
+      this.passwordFail = true;
+    } else {
+      this.passwordFail = false;
+      this.registerUser();
+    }
+
+  }
   
-  
+  registerUser(){
+   let obj = {"email":this.email, "password":this.password1}
+    this.postRegisterUserDataFire("/users", obj)
+  }
+
+  postRegisterUserDataFire(url, obj){
+        const itemObservable = this.af.database.list(url);
+        itemObservable.push(obj).catch((e)=> console.error(e.message) );
+        this.router.navigate(['/userprofile']);
+    }
 
   cancel(){
     this.router.navigate(['/admin/login']);
--- src/app/user/userProfile/userprofile.component.ts
+++ src/app/user/userProfile/userprofile.component.ts
@@ -22,46 +22,24 @@ export class userProfileComponent  {
      phonenumber: number =0;
      address: string = "";
      item: any;
+     uid: any;
 
-
-	  constructor(private af: AngularFire, private router: Router){
-
-	  	  this.item = af.database.object('userProfile/', { preserveSnapshot: true });
-			this.item
-			  .subscribe(snapshots =>
-			   {
-			    snapshots.forEach(snapshot => {
-			      this.userData = snapshot.val()
-			    });
-			  })
-		   
-		   console.log(this.userData);
-  		}
+     constructor(public af: AngularFire,private router: Router) {
+    	this.af.auth.subscribe(auth => this.uid=auth.uid);
+    	this.getPost();
+  	 }
+	  
 	 OnInit() {
 
 	 }
+	 
 	 getPost() {
 
-	   let dbRef = this.af.database.object('/userProfile');
-		
+	   let dbRef = this.af.database.object('/users/'+this.uid, 
+	   	{ preserveSnapshot: true });
+	    dbRef.subscribe(snapshot => {
+  			console.log(snapshot.val())
+		});
+	}
 
-
-	 }
-
-	createPost(){
-		alert("create post")
-        let storageRef = firebase.storage().ref();
-
-                let dbRef = firebase.database().ref('userProfile/');
-                let newPost = dbRef.push();
-                newPost.set ({
-                    name: this.name,
-                    address: this.address,
-                    phonenumber: this.phonenumber,
-                    id: newPost.key
-                })
-            .catch ((error)=>{
-                alert(`failed upload: ${error}`);
-            });
-    }
 }
