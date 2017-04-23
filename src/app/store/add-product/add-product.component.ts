6b21b718c86389c7a3684d3d4ccaacf8776fe74d
Veeresh Hiremath <vhiremath4@sapient.com>
2017-04-23 22:53:21 +0530

    Add shop

--- src/app/store/add-product/add-product.component.ts
+++ src/app/store/add-product/add-product.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 @Input() shop;
+  public productName: string ="";
+  public productquantity: number =0;
+  public productType: string ="";
   constructor() { }
 
   ngOnInit() {
