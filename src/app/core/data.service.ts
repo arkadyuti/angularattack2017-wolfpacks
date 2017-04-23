a442092be27f71ce3c604a7627eca921c3219f25
Saurabh Sinha <ssi253@sapient.com>
2017-04-24 04:04:20 +0530

    Build Conflicts

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-// import { Observable } from 'rxjs/observable';
-// import { Observer } from 'rxjs/Observer';
-// import 'rxjs/Rx';
+import { Observable } from 'rxjs/observable';
+import { Observer } from 'rxjs/Observer';
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataService {
